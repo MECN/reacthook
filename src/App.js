268b38aa17a1042166d6c13ce66cea12a3ff9b04
czeqin <czeqin@cyberway.net.cn>
2019-09-23 14:03:27 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+function Index() {
+  useEffect(() => {
+    console.log('useEffect=>老弟，你来了！Index页面')
+    return () => {
+      console.log('老弟，你走了!Index页面')
+    }
+  }
+  )
+  return (
+    <h2>MECN</h2>
+  )
+}
+function List() {
+  useEffect(() => {
+    console.log('useEffect=>小老弟，你来了')
+  })
+  return (
+    <h2>List</h2>
+  )
+}
 function App() {
+  const [count, setCount] = useState(0)
+  const [num, setNum] = useState(5)
+  useEffect(() => {
+    console.log(`count${count}`)
+  },[count])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>clicked {count} times</p>
+      <button onClick={() => { setCount(count + 1) }}>click me</button>
+      <p>clicked {num}</p>
+      <button onClick={() => { setNum(num + 1) }}>click me</button>
+      <Router>
+        <ul>
+          <li><Link to="/">首页</Link></li>
+          <li><Link to="/list/">列表</Link></li>
+        </ul>
+        <Route path="/" exact component={Index}></Route>
+        <Route path="/list/" exact component={List}></Route>
+      </Router>
     </div>
   );
 }
+
 
 export default App;
